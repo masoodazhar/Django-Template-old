46ff41bd4b81b9ba31fd22c15c0900a4cf8b21a0
kleberbaum <kleberbaum@erebos.xyz>
2019-07-27 18:54:56 +0200

    break down the settings into base, development and production The development settings are unsuitable for production. See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/

---
+++ esite/settings/development.py
@@ -0,0 +1,21 @@
+"""
+Django development settings for esite project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/2.2/ref/settings/
+
+This development settings are unsuitable for production, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+"""
+
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'ct*z11t*ns876z)!f5f3h1byn7pp1ma5i!9*oo!=dmtmnrvzcn'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
---
+++ esite/settings/production.py
@@ -0,0 +1,17 @@
+"""
+Django production settings for esite project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/2.2/ref/settings/
+
+This development settings are unsuitable for production, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+"""
+
+from .base import *
+
+
+ALLOWED_HOSTS = []
