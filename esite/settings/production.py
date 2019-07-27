886f1d2aed926799f148fa8e0721884bdc0d3e31
kleberbaum <kleberbaum@erebos.xyz>
2019-07-27 19:51:29 +0200

    add dynamic hostname list in production Accept all hostnames, since we don't know in advance which hostname will be used for any given Docker instance.

--- esite/settings/production.py
+++ esite/settings/production.py
@@ -14,7 +14,10 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 from .base import *
 
 
+# Accept all hostnames, since we don't know in advance
+# which hostname will be used for any given Docker instance.
 # IMPORTANT: Set this to a real hostname when using this in production!
 # See https://docs.djangoproject.com/en/1.10/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(';')
 
